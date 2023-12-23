b80a1df1a358e48ca134d4c936413f6eb9e6edcf
BoloSalgadoo <ttf1880@gmail.com>
2023-12-23 15:35:35 -0300

    feat: initial config

---
+++ src/main/java/com/paymentServiceChallenge/PaymentServiceChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.paymentServiceChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentServiceChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentServiceChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paymentServiceChallenge/PaymentServiceChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymentServiceChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
