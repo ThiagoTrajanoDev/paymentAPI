89d62740c7ccdf8d47ac94b009d8e95ae55dd1d9
BoloSalgadoo <ttf1880@gmail.com>
2023-12-23 15:37:57 -0300

    feat: create transaction entity

---
+++ src/main/java/com/paymentServiceChallenge/domain/transaction/Transaction.java
@@ -0,0 +1,36 @@
+package com.paymentServiceChallenge.domain.transaction;
+
+import com.paymentServiceChallenge.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name="transactions")
+@Table(name="transactions")
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+@AllArgsConstructor
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal amount;
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+    private LocalDateTime timestamp;
+
+
+
+
+}
